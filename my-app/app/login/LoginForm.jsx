8c89b6a559129860c3fcb3c1df01ffc1bdadef2a
shell-lab <ikhtyarhussain156676@gmail.com>
2025-10-16 11:01:17 +0530

    login credentials now fetching successfully

--- my-app/app/login/LoginForm.jsx
+++ my-app/app/login/LoginForm.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+"use client";
+
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+
+      if (res.error) {
+        setError("Invalid Credentials");
+        return;
+      }
+
+      router.replace("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <form className="space-y-4">
+    <form onSubmit={handleSubmit} className="space-y-4">
       <div>
-        <label htmlFor="email" className="block text-xs font-semibold text-gray-500 uppercase mb-1">
+        <label
+          htmlFor="email"
+          className="block text-xs font-semibold text-gray-500 uppercase mb-1"
+        >
           Email
         </label>
         <input
@@ -13,12 +47,16 @@ export default function LoginForm() {
           type="email"
           autoComplete="email"
           required
+          onChange={(e) => setEmail(e.target.value)}
           className="w-full p-3 bg-gray-100 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-black placeholder-gray-400"
         />
       </div>
 
       <div>
-        <label htmlFor="password" className="block text-xs font-semibold text-gray-500 uppercase mb-1">
+        <label
+          htmlFor="password"
+          className="block text-xs font-semibold text-gray-500 uppercase mb-1"
+        >
           Password
         </label>
         <input
@@ -27,9 +65,16 @@ export default function LoginForm() {
           type="password"
           autoComplete="current-password"
           required
+          onChange={(e) => setPassword(e.target.value)}
           className="w-full p-3 bg-gray-100 border border-gray-200 rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
         />
       </div>
+
+      {error && (
+        <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">
+          {error}
+        </div>
+      )}
 
       <button
         type="submit"
